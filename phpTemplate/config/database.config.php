94de7d86f971d08e745603ea23c22be08c8c315d
CatalystDP <cld18874@126.com>
2014-02-25 20:47:03 +0800

    lastcommit

---
+++ phpTemplate/config/database.config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dp
+ * Date: 14-2-24
+ * Time: 下午1:25
+ */
+    define("TYPE","mysql");
+    define("USER","root");
+    define("PASSWORD","000");
+    define("HOST","localhost");
+    define("DATABASE","fleetlinux");
+?>
