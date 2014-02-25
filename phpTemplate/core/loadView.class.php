7362364805346a761e14fa8fe45a2754ef980fba
CatalystDP <cld18874@126.com>
2014-02-26 00:06:09 +0800

    lastcommit

--- phpTemplate/core/loadView.class.php
+++ phpTemplate/core/loadView.class.php
@@ -1,23 +1,31 @@
 <?php
 include_once "config/path.config.php";
-final class LoadView{
-        private $v;
-        public function view($viewPath,$viewClass,$viewArg){
-            /*
-             *@param string $viewPath view路径
-             *@param string $viewClass view类名
-             *@param array $viewArg 传递给view的变量
-             *@return bool view是否载入成功*/
-            if(func_num_args()==0)
-                return false;
-            if(include_once("view/".$viewPath.".php"))
-                $this->v=new $viewClass($viewArg);
-            else
-                self::load_404();
-            //echo "view".include_once("view/".$viewPath.".php");
-        }
-        public static function load_404(){
-            include_once "view/404/page_404.html";
-        }
+
+final class LoadView
+{
+    private $v;
+
+    public function view($viewPath, $viewClass, $viewFunc, $viewArg)
+    {
+        /*
+         *@param string $viewPath view路径
+         *@param string $viewClass view类名
+         *@param array $viewArg 传递给view的变量
+         *@return bool view是否载入成功*/
+        if (func_num_args() == 0)
+            return false;
+        if (include_once("view/" . $viewPath . ".php")) {
+            $this->v = new $viewClass($viewArg);
+            $this->v->$viewFunc();
+        } else
+            self::load_404();
+        //echo "view".include_once("view/".$viewPath.".php");
     }
+
+    public static function load_404()
+    {
+        include_once "view/404/page_404.html";
+    }
+}
+
 ?>
